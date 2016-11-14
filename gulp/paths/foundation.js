12d8672c679c0487afd5b126d880c5200d34994d
Sergey Rubets <sramator@gmail.com>
2016-11-14 16:39:11 +0200

    First commit

---
+++ gulp/paths/foundation.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Array of paths to executable files. Relatively of 'node_modules' directory.
+ * @module gulp/path/foundation
+ * @type {String[]}
+ */
+
+module.exports = [
+  'jquery/dist/jquery.min.js'
+];
---
+++ gulp/paths/sass.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  app: [
+    'source/style/app.scss'
+  ],
+
+  foundation: [
+    'normalize.scss/nomalize.scss'
+  ]
+};
